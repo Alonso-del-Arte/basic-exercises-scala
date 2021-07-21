4cbf02c892a651a0d148eb80c2014cb8a55a1de5
Alonso del Arte <alonso.delarte@gmail.com>
2021-07-21 13:15:52 -0400

    Now passing case insensitive test

--- src/textops/CharacterCounter.scala
+++ src/textops/CharacterCounter.scala
@@ -13,9 +13,17 @@ class CharacterCounter(val text: String) {
     }
   }
 
-  // THIS WILL FAIL THE caseSensitivity = false TEST
-  def query(ch: Char, caseSensitivity: Boolean = true): Int =
+  private def queryCaseSensitive(ch: Char): Int = {
     if (this.charMap.contains(ch)) this.charMap(ch) else 0
+  }
+
+  def query(ch: Char, caseSensitivity: Boolean = true): Int = {
+    if (caseSensitivity || !ch.isLetter) queryCaseSensitive(ch) else {
+      val lower = ch.toLower
+      val upper = ch.toUpper
+      queryCaseSensitive(lower) + queryCaseSensitive(upper)
+    }
+  }
 
   def results: HashMap[Char, Int] = this.charMap
 
